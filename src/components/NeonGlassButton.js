a23f06b72e58c8a7e36d3482d283cc54761cfabf
Wilhelm Hattenbach <wilhelm.hattenbach@yh.nackademin.se>
2021-04-14 08:31:54 +0200

    First commit

---
+++ src/components/NeonGlassButton.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './CSS/style.css'
+
+function neonGlassButton() {
+    return (
+        <div className="container">
+            <div className="btn"><a href="#">Read More</a></div>
+            <div className="btn"><a href="#">Read More</a></div>
+            <div className="btn"><a href="#">Read More</a></div>
+        </div>
+    );
+}
+
+export default neonGlassButton;
